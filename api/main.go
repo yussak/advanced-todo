a4ba50a84d4e89be32e898f5c491d48c7a6313a2
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-07-27 22:14:50 +0900

    enable to connect to dbd

--- api/main.go
+++ api/main.go
@@ -1,24 +1,43 @@
 package main
 
 import (
-	"net/http"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
 
-	"github.com/rs/cors"
+	_ "github.com/lib/pq"
 )
 
+var db *sql.DB
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.Write([]byte("hello world"))
-	})
+	// mux := http.NewServeMux()
+	// mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Header().Set("Content-Type", "application/json")
+	// 	w.Write([]byte("hello world"))
+	// })
 
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},
-		AllowCredentials: true,
-		Debug:            true,
-	})
+	// c := cors.New(cors.Options{
+	// 	AllowedOrigins:   []string{"http://localhost:3000"},
+	// 	AllowCredentials: true,
+	// 	Debug:            true,
+	// })
 
-	handler := c.Handler(mux)
-	http.ListenAndServe(":8080", handler)
+	// handler := c.Handler(mux)
+	// http.ListenAndServe(":8080", handler)
+
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DBNAME"))
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("connection established!")
 }
