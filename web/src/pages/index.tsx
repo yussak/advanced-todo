fa8926df5cf8a9e974ec24823655d0beb6aa1db8
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-08-06 20:53:37 +0900

    feat: reset after submit

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -34,6 +34,7 @@ export default function Home() {
       console.error(error);
     }
     await fetchTodos();
+    reset();
   };
 
   type Inputs = {
@@ -54,6 +55,7 @@ export default function Home() {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = async (data) => await addTodo(data);
 
