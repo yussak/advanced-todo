f4cf47b14673e382afca4e1e7c297f4e33328892
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-08-10 04:19:19 +0900

    style: change color

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -75,7 +75,7 @@ export default function Home() {
         {/* TODO:出る時ずれるので修正 */}
         {flashMessage && (
           <div
-            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
             role="alert"
           >
             <strong className="font-bold">{flashMessage}</strong>
