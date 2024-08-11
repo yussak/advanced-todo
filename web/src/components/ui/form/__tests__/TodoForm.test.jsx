65b7dc7d0028a6967a45050a1d9d8bbc4345af19
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-08-11 16:19:27 +0900

    test: add test

--- web/src/components/ui/form/__tests__/TodoForm.test.jsx
+++ web/src/components/ui/form/__tests__/TodoForm.test.jsx
@@ -2,18 +2,29 @@ import { it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import TodoList from "../../../todos/TodoList";
 import axios from "axios";
+import "@testing-library/jest-dom";
 
 vi.mock("axios");
 
-it("個数が正しい", async () => {
-  // apiとのやり取りは親側でやっているので、TodoListコンポーネントのテストでは直書きでtodosでOK
-  const todos = [
-    { id: "abc", title: "title1", body: "body1" },
-    { id: "def", title: "title2", body: "body2" },
-  ];
-  axios.get.mockResolvedValue({ data: todos });
-  render(<TodoList todos={todos} />);
+describe("TodoForm", () => {
+  it("個数が正しい", async () => {
+    // apiとのやり取りは親側でやっているので、TodoListコンポーネントのテストでは直書きでtodosでOK
+    const todos = [
+      { id: "abc", title: "title1", body: "body1" },
+      { id: "def", title: "title2", body: "body2" },
+    ];
+    axios.get.mockResolvedValue({ data: todos });
+    render(<TodoList todos={todos} />);
 
-  const todoItems = screen.getAllByRole("listitem");
-  expect(todoItems).toHaveLength(todos.length);
+    const todoItems = screen.getAllByRole("listitem");
+    expect(todoItems).toHaveLength(todos.length);
+  });
+
+  it("should display text when todos are empty", async () => {
+    const todos = [];
+    const onDelete = () => {};
+    render(<TodoList todos={todos} onDelete={onDelete} />);
+
+    expect(screen.getByText("not found")).toBeInTheDocument();
+  });
 });
