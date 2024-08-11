8ce3e5fbfeb438ca28ac913adc64df77d872a9e8
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-08-11 16:10:16 +0900

    feat: add todo not found text

--- web/src/components/todos/TodoList.tsx
+++ web/src/components/todos/TodoList.tsx
@@ -3,13 +3,17 @@ import Link from "next/link";
 const TodoList = ({ todos, onDelete }) => {
   return (
     <div>
-      {todos.map((todo) => (
-        <p key={todo.id} role="listitem">
-          {todo.title}, {todo.body}
-          <button onClick={() => onDelete(todo.id)}>delete</button>
-          <Link href={`/todos/${todo.id}`}>detail</Link>
-        </p>
-      ))}
+      {todos.length > 0 ? (
+        todos.map((todo) => (
+          <p key={todo.id} role="listitem">
+            {todo.title}, {todo.body}
+            <button onClick={() => onDelete(todo.id)}>delete</button>
+            <Link href={`/todos/${todo.id}`}>detail</Link>
+          </p>
+        ))
+      ) : (
+        <p>not found</p>
+      )}
     </div>
   );
 };
