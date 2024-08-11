4264c4a4e3492f96a1a0327d5a08261ed52bdeb8
YusukeSakuraba <yusuke.sakuraba0213@gmail.com>
2024-08-11 09:06:56 +0900

    test: add test

---
+++ web/src/tests/todos.unit.test.jsx
@@ -0,0 +1,19 @@
+import { it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+import axios from "axios";
+
+vi.mock("axios");
+
+it("個数が正しい", async () => {
+  // apiとのやり取りは親側でやっているので、TodoListコンポーネントのテストでは直書きでtodosでOK
+  const todos = [
+    { id: "abc", title: "title1", body: "body1" },
+    { id: "def", title: "title2", body: "body2" },
+  ];
+  axios.get.mockResolvedValue({ data: todos });
+  render(<TodoList todos={todos} />);
+
+  const todoItems = screen.getAllByRole("listitem");
+  expect(todoItems).toHaveLength(todos.length);
+});
